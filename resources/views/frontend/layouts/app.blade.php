29256c30b7f99df21ace0bc01d3cb234f4b09744
MurariThakur <murarithakur32@gmail.com>
2025-08-05 19:58:46 +0530

    landing page setup

---
+++ resources/views/frontend/layouts/app.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<!-- molla/index-2.html  22 Nov 2019 09:55:32 GMT -->
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Ecommerce</title>
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Favicon -->
+
+    <link rel="shortcut icon" href="frontend/assets/images/icons/favicon.ico">
+
+    <!-- Plugins CSS File -->
+    <link rel="stylesheet" href="frontend/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="frontend/assets/css/plugins/owl-carousel/owl.carousel.css">
+    <link rel="stylesheet" href="frontend/assets/css/plugins/magnific-popup/magnific-popup.css">
+    <!-- Main CSS File -->
+    <link rel="stylesheet" href="frontend/assets/css/style.css">
+</head>
+
+<body>
+    <div class="page-wrapper">
+
+        @include('frontend.layouts.header')
+        @yield('content')
+        @include('frontend.layouts.footer')
+    </div><!-- End .page-wrapper -->
+    <button id="scroll-top" title="Back to Top"><i class="icon-arrow-up"></i></button>
+
+    <!-- Mobile Menu -->
+    <div class="mobile-menu-overlay"></div><!-- End .mobil-menu-overlay -->
+
+    @include('frontend.layouts.mobile_menu')
+    <!-- Sign in / Register Modal -->
+@include('frontend.layouts.signup')
+
+
+    {{-- <div class="container newsletter-popup-container mfp-hide" id="newsletter-popup-form">
+        <div class="row justify-content-center">
+            <div class="col-10">
+                <div class="row no-gutters bg-white newsletter-popup-content">
+                    <div class="col-xl-3-5col col-lg-7 banner-content-wrap">
+                        <div class="banner-content text-center">
+                            <img src="frontend/assets/images/popup/newsletter/logo.png" class="logo" alt="logo"
+                                width="60" height="15">
+                            <h2 class="banner-title">get <span>25<light>%</light></span> off</h2>
+                            <p>Subscribe to the Molla eCommerce newsletter to receive timely updates from your favorite
+                                products.</p>
+                            <form action="#">
+                                <div class="input-group input-group-round">
+                                    <input type="email" class="form-control form-control-white"
+                                        placeholder="Your Email Address" aria-label="Email Adress" required>
+                                    <div class="input-group-append">
+                                        <button class="btn" type="submit"><span>go</span></button>
+                                    </div><!-- .End .input-group-append -->
+                                </div><!-- .End .input-group -->
+                            </form>
+                            <div class="custom-control custom-checkbox">
+                                <input type="checkbox" class="custom-control-input" id="register-policy-2" required>
+                                <label class="custom-control-label" for="register-policy-2">Do not show this popup
+                                    again</label>
+                            </div><!-- End .custom-checkbox -->
+                        </div>
+                    </div>
+                    <div class="col-xl-2-5col col-lg-5 ">
+                        <img src="frontend/assets/images/popup/newsletter/img-1.jpg" class="newsletter-img"
+                            alt="newsletter">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> --}}
+
+    <!-- Plugins JS File -->
+    <script src="frontend/assets/js/jquery.min.js"></script>
+    <script src="frontend/assets/js/bootstrap.bundle.min.js"></script>
+    <script src="frontend/assets/js/jquery.hoverIntent.min.js"></script>
+    <script src="frontend/assets/js/jquery.waypoints.min.js"></script>
+    <script src="frontend/assets/js/superfish.min.js"></script>
+    <script src="frontend/assets/js/owl.carousel.min.js"></script>
+    <script src="frontend/assets/js/jquery.magnific-popup.min.js"></script>
+    <!-- Main JS File -->
+    <script src="frontend/assets/js/main.js"></script>
+
+</body>
+
+</html>
